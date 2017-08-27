bfbc8d32f2d349ec774ba55a5be8799cfb2b4adf
alexNeto <alex.neto3@hotmail.com>
2017-08-27 22:17:11 +0000

    add space, minus and pipe

--- grid_display.py
+++ grid_display.py
@@ -4,15 +4,15 @@ import random
 
 class GridDisplay:
 
-    displaytext = []
     color = ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127']
     frame = ''
     font = Fonthub()
 
     def __init__(self, frame):
-        #frame.geometry("676x104")
         self.frame = frame
-        self.createdisplay(self.displaytext)
+        self.cleandisplay()
+
+    def cleandisplay(self):
         for i in range(53):
             for j in range(7):
                 bg_color = self.color[0]
@@ -22,11 +22,13 @@ class GridDisplay:
                 l.place(x=20 + i * 12, y=20 + j * 12, width=10, height=10)
 
     def createdisplay(self, stringtext=""):
-
-        self.displaytext = self.font.string2fonthub(stringtext)
+        self.cleandisplay()
+        displaytext = self.font.string2fonthub(stringtext)
         week = 0
         j = 0
-        for i, ni in enumerate(self.displaytext):
+        for i, ni in enumerate(displaytext):
+            if week >= 53:
+                break
             if i % 7 == 0:
                 week += 1
                 j = 0
@@ -34,20 +36,8 @@ class GridDisplay:
                 bg_color = self.color[random.randrange(1, 5)]
             else:
                 bg_color = self.color[0]
-            l = Label(self.frame,
-                          text=' ',
-                          bg=bg_color)
+            l = Label(self.frame, text=' ', bg=bg_color)
             l.place(x=20 + week * 12, y=20 + j * 12, width=10, height=10)
             j += 1
 
-
-        '''
-        for i in range(53):
-            for j in range(7):
-                bg_colour = self.color[0]
-                l = Label(self.frame,
-                          text=' ',
-                          bg=bg_colour)
-                l.place(x=20 + i * 12, y=20 + j * 12, width=10, height=10)
-    '''
 
